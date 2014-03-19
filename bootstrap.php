225a9a1864f34ad293d9ec70987fceccb453ac19
Carles Climent <carlescliment@hotmail.com>
2014-03-19 17:07:13 +0100

    Fixed configuration of phpunit

--- bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
-$loader = require_once(__DIR__.'/vendor/autoload.php');
-
-AnnotationRegistry::registerFile(__DIR__."/vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php");
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
